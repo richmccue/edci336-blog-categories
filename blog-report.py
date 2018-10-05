6bf81b267f09cb8b560baf5f51f9d33690619883
Ryan McCue <ryan@msys.ca>
2018-10-04 22:51:34 -0700

    Add dockerfile

--- blog-report.py
+++ blog-report.py
@@ -3,22 +3,25 @@
 from bs4 import BeautifulSoup
 from os import system
 
-sites = []
-all_categories =[]
-stats =[]
+def main():
+    sites = []
+    all_categories = []
+    stats = []
 
-for site in sites:
-    stats[site] = {}
-    #get feed
-    #pass into bs4
-    for item in site.getAll('item'):
-        for category in item.getAll('category'):
-            # regex it
-            stats.append(category)
-    calc = {}
-    for item in stats:
-        if calc[item] != None:
-            calc[item] = 0
-        calc[item] += 1
-    for category in all_categories:
-        output.append("<td>" + calc[category] + "</td>")
+    for site in sites:
+        stats[site] = {}
+        #get feed
+        #pass into bs4
+        for item in site.getAll('item'):
+            for category in item.getAll('category'):
+                # regex it
+                stats.append(category)
+        calc = {}
+        for item in stats:
+            if calc[item] != None:
+                calc[item] = 0
+            calc[item] += 1
+        for category in all_categories:
+            output.append("<td>" + calc[category] + "</td>")
+
+main()
