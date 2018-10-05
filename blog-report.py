aee49fd9a6c801b4b7481925696c519beec9835e
Rich McCue <rich@msys.ca>
2018-10-04 20:37:53 -0700

    add blog-report.py file

---
+++ blog-report.py
@@ -0,0 +1,21 @@
+
+
+sites = []
+all_categories =[]
+stats =[]
+
+for site in sites:
+    stats[site] = {}
+    #get feed
+    #pass into bs4
+    for item in site.getAll('item'):
+        for category in item.getAll('category'):
+            # regex it
+            stats.append(category)
+    calc = {}
+    for item in stats:
+        if calc[item] != None:
+            calc[item] = 0
+        calc[item] += 1
+    for category in all_categories:
+        output.append("<td>" + calc[category] + "</td>")
