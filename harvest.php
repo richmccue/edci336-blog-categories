65519f628c1e5c2bb10865b8f4c05a146fd43432
Rich McCue <rich@msys.ca>
2018-10-27 16:37:52 -0700

    exploded categories for each post in harvest.php

---
+++ harvest.php
@@ -0,0 +1,45 @@
+<?php
+  require('db.php');
+?>
+<html>
+  <head>
+    <title>Harvest New EDCI 336 Blog Posts</title>
+  </head>
+  <body>
+    <?php
+      //Feed URLs
+      $feeds = array(
+          "https://brechanbird.wordpress.com/feed",
+          "https://hilarygraham11.wordpress.com/feed"
+      );
+
+      //Read each feed's items
+      $entries = array();
+      foreach($feeds as $feed) {
+          $xml = simplexml_load_file($feed);
+          $entries = array_merge($entries, $xml->xpath("//item"));
+      }
+
+      //Sort feed entries by pubDate
+      usort($entries, function ($feed1, $feed2) {
+          return strtotime($feed2->pubDate) - strtotime($feed1->pubDate);
+      });
+
+      ?>
+
+      <ul><?php
+      //Print all the entries
+      foreach($entries as $entry){
+          ?>
+          <li><a href="<?= $entry->link ?>"><?= $entry->title ?></a> (<?= parse_url($entry->link)['host'] ?>)
+          <p><?= strftime('%m/%d/%Y %I:%M %p', strtotime($entry->pubDate)) ?></p>
+          <p><?= $entry->description ?></p>
+          <p>Categories: <?= implode('</strong>, <strong>', (array)$entry->category) ?></p></li>
+          <?php
+      }
+      mysqli_close($db);
+      ?>
+      </ul>
+
+  </body>
+</html>
