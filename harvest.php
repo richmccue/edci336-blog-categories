84800dcba5d18cf2b8c654a563bd83bfa9b8c77f
Rich McCue <rich@msys.ca>
2018-11-01 19:57:44 -0700

    part way upgrading harvest.php to use mysql db

--- harvest.php
+++ harvest.php
@@ -14,17 +14,12 @@
       if (mysqli_num_rows($result) > 0) {
           // output data of each row
           while($row = mysqli_fetch_assoc($result)) {
-            echo 'url: ' . $row["blog_url"] . '</br>';
+            # echo 'url: ' . $row["blog_url"] . '</br>';
             $feeds[] = $row["blog_url"] . '/feed/';
           }
       } else {
           echo "0 results";
       }
-
-      # $feeds = array(
-      #    "https://kennedyhumeniuk.wordpress.com/feed/",
-          #"https://benston789106021.wordpress.com/feed"
-      #);
 
       //Read each feed's items
       $entries = array();
@@ -34,14 +29,28 @@
       }
 
       //Sort feed entries by pubDate
-      usort($entries, function ($feed1, $feed2) {
-          return strtotime($feed2->pubDate) - strtotime($feed1->pubDate);
-      });
-
+      # usort($entries, function ($feed1, $feed2) {
+      #    return strtotime($feed2->pubDate) - strtotime($feed1->pubDate);
+      #});
       ?>
 
       <ul><?php
-      //Print all the entries
+      //Add all new blog posts into the posts table
+      foreach($entries as $entry){
+        $sql = "SELECT * FROM posts WHERE post_url = " . $entry->link;
+        $result = mysqli_query($db, $sql);
+
+        if (mysqli_num_rows($result) > 0) {
+            echo "already added";
+        } else {
+          #add new blog post to "posts" table
+          while($row = mysqli_fetch_assoc($result)) {
+            $feeds[] = $row["blog_url"] . '/feed/';
+          }
+        }
+      }
+
+      //Print all posts on web page.
       foreach($entries as $entry){
           ?>
           <li><a href="<?= $entry->link ?>"><?= $entry->title ?></a>
