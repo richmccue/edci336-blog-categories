6aaf0a9c7b93d21030877426623a344787ce6513
Rich McCue <rich@msys.ca>
2018-11-13 16:07:42 -0800

    Starting to work on adding the blog_id to the post for easier lookup of posts from specific blogs

--- harvest.php
+++ harvest.php
@@ -14,8 +14,7 @@
       if (mysqli_num_rows($result) > 0) {
           // output data of each row
           while($row = mysqli_fetch_assoc($result)) {
-            $feeds[] = $row["blog_url"] . '/feed/';
-            $blog_id[] = $row["blog_id"];
+            $feeds[$row["blog_id"]] = $row["blog_url"] . '/feed/';
           }
       } else {
           echo "0 results";
@@ -42,10 +41,15 @@
           #add new blog post to "posts" table
           $categories = implode(', ', (array)$entry->category);
           $pubdate = date('Y-m-d',strtotime($entry->pubDate));
-          $insert_post = "INSERT INTO posts (post_url, categories, date)
-            VALUES ('$entry->link', '$categories','$pubdate')";
+
+          # lookup 'blog_id' from 'blogs' table for this blog posts
+          $blogs_sql = "SELECT * FROM blogs WHERE post_url = '" . $entry->link . "'";
+          $result = mysqli_query($db, $blogs_sql);
+
+          $insert_post = "INSERT INTO posts (post_url, categories, date, blog_id)
+            VALUES ('$entry->link', '$categories','$pubdate',$blog_id)";
             if (mysqli_query($db, $insert_post)) {
-                echo "New record created successfully";
+                echo "<b>New record created successfully</b><br />";
             } else {
                 echo "Error: " . $insert_post . " - " . mysqli_error($db);
             }
