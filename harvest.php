fc769891f128f782220d5219ad1d2c7d9be6648a
Rich McCue <rich@msys.ca>
2018-11-13 16:18:00 -0800

    harvest.php more work on adding blog_id to posts table

--- harvest.php
+++ harvest.php
@@ -41,11 +41,14 @@
           #add new blog post to "posts" table
           $categories = implode(', ', (array)$entry->category);
           $pubdate = date('Y-m-d',strtotime($entry->pubDate));
-
+###############
           # lookup 'blog_id' from 'blogs' table for this blog posts
-          $blogs_sql = "SELECT * FROM blogs WHERE post_url = '" . $entry->link . "'";
-          $result = mysqli_query($db, $blogs_sql);
-
+          $blog_post_url = $entry->link;  ## Need to finish this!!!!
+          $blogs_sql = "SELECT * FROM blogs WHERE blog_url = '" . $blog_post_url . "'";
+          $blogs_result = mysqli_query($db, $blogs_sql);
+          $blog_id = $blogs_result["blog_id"];
+###############
+          # Insert...
           $insert_post = "INSERT INTO posts (post_url, categories, date, blog_id)
             VALUES ('$entry->link', '$categories','$pubdate',$blog_id)";
             if (mysqli_query($db, $insert_post)) {
