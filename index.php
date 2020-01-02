0939438e2b583136a6effc8bf3f62a62022ad529
Rich McCue <rich@msys.ca>
2020-01-02 12:00:34 -0800

    index.php displays all sections. section.php displays all blogs for a section.

--- index.php
+++ index.php
@@ -8,21 +8,20 @@
  <h1>EDCI 336 Sections</h1>
 
 <table>
-  <tr><td><b>id</b></td><td><b>Section</b></td><td><b>Instructor</b></td></tr>
- <?php
+
+  <tr><td><b>Section</b></td><td><b>Instructor</b></td></tr>
+<?php
  //Step2
- $query = "SELECT * FROM sections";
+ $query = "SELECT * FROM section ORDER BY section DESC";
  mysqli_query($db, $query) or die('Error querying database.');
 
  //Step3
  $result = mysqli_query($db, $query);
- $row = mysqli_fetch_array($result);
 
- while ($row = mysqli_fetch_array($result)) {
-
-  echo '<tr><td>' . $row['section_id'] . '</td><td><a href="' .
-    'http://msystems.net/edci336/section.php?id='. $row['section_id'] . ">' .
-    $row['section'] . '</a></td><td>' . $row['intructor'] .'</td></tr>';
+ while ($row = mysqli_fetch_assoc($result)) {
+  echo '<tr><td><a href="' .
+    'http://msystems.net/edci336/section.php?section='. $row['section'] . '">' .
+    $row['section'] . '</a></td><td>' . $row['instructor'] .'</td></tr>';
  }
  //Step 4
  mysqli_close($db);
--- section.php
+++ section.php
@@ -1,29 +1,29 @@
 <?php
   require('db.php');
+  $section = $_GET["section"];
 ?>
 <html>
  <head>
  </head>
  <body>
- <h1>EDCI 336 A03 Blogs</h1>
+ <h1>EDCI 336 <? echo $section; ?></h1>
 
 <table>
-  <tr><td><b>id</b></td><td><b>URL</b></td><td><b>Inquiries</b></td></tr>
+  <tr><td><b>URL</b></td><td><b>Inquiries</b></td></tr>
  <?php
  //Step2
- $query = "SELECT * FROM blogs";
+ $query = "SELECT * FROM blogs WHERE section='". $section . "'";
  mysqli_query($db, $query) or die('Error querying database.');
 
  //Step3
  $result = mysqli_query($db, $query);
- $row = mysqli_fetch_array($result);
 
- while ($row = mysqli_fetch_array($result)) {
+ while ($row = mysqli_fetch_assoc($result)) {
   // Check, if not have http:// or https:// then prepend it
   $blog_url_no_http = $row['blog_url'];
   $blog_url_no_http = preg_replace( "#^[^:/.]*[:/]+#i", "", $blog_url_no_http );
 
-  echo '<tr><td>' . $row['blog_id'] . '</td><td><a href="' .
+  echo '<tr><td><a href="' .
     'http://msystems.net/edci336/harvest-orig.php?id='. $row['blog_id'] . '&' .
     'blog_url=' . $blog_url_no_http . '/feed/">' .
     $row['blog_url'] . '</a></td><td>' . $row['comment'] .'</td></tr>';
