143028ca86a58eb1e7d9f1d8abc3fb1bc5fb24fd
Rich McCue <rich@msys.ca>
2020-01-03 10:47:30 -0800

    grade-update.php fixed html in php

--- grade-post.php
+++ grade-post.php
@@ -9,14 +9,11 @@
  <h1>EDCI 336 Blog Tracker - Grade Post</h1>
 
 <?php
- //Step2
- $query = "SELECT * FROM posts WHERE post_id =" . $post_id;
- mysqli_query($db, $query) or die(mysqli_error());
+$query = "SELECT * FROM posts WHERE post_id =" . $post_id;
+mysqli_query($db, $query) or die(mysqli_error());
+$result = mysqli_query($db, $query);
 
- //Step3
- $result = mysqli_query($db, $query);
-
-echo "<div class='form'>";
+echo "<form name='form' method='post' action=''>";
 
 # If the form was submitted update posts table with the grade assigned.
 if(isset($_POST['new']) && $_POST['new']==1) {
@@ -25,15 +22,16 @@ if(isset($_POST['new']) && $_POST['new']==1) {
   echo "<p style='color:#FF0000;''>Record Updated Successfully!</p>";
 } else {
 #If no grade was assigned, display current grade (if any).
-   while ($row = mysqli_fetch_assoc($result)) {
-    echo "<p><a href='" . $row['post_url'] . "' target='_blank'>" . $row['post_title'] . "</a></p>";
-    echo '<p>Grade: ' . $row['grade'] . '</p>';
-    echo '<p>Notes: ' . $row['notes'] . '</p>';
-   }
+  while ($row = mysqli_fetch_assoc($result)) {
+  echo "<p><a href='" . $row['post_url'] . "' target='_blank'>" . $row['post_title'] . "</a></p>";
+  echo '<p>Grade: ' . $row['grade'] . '</p>';
+  echo '<p>Notes: ' . $row['notes'] . '</p>';
+  echo '<p>Post ID: ' . $row['post_id'] . '</p>';
+  }
 }
 
-</form>
- //Step 4
+echo "</form>";
+
  mysqli_close($db);
  ?>
 
