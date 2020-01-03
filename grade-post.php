435a689668037ce79cf73f7b0bb45525f7f06d16
Rich McCue <rich@msys.ca>
2020-01-03 11:59:24 -0800

    Made it possible to navigate from the grade page to the blog post page.

--- grade-post.php
+++ grade-post.php
@@ -1,6 +1,9 @@
 <?php
   require('db.php');
   $post_id = $_GET["post_id"];
+  $blog_id = $_GET["blog_id"];
+  $blog_url = $_GET["blog_url"];
+  $section = $_GET["section"];
 ?>
 <html>
  <head>
@@ -21,15 +24,18 @@ if(isset($_POST['new']) && $_POST['new']==1) {
     "' WHERE post_id=" . $_REQUEST['post_id'];
   mysqli_query($db, $update) or die(mysqli_error());
   echo "<p style='color:#FF0000;''>Record Updated Successfully!</p>";
+  echo "<a href='harvest-orig.php?id=" . $blog_id . "&section=" . $section
+    ."&blog_url=" . $blog_url . "'>Return to student's blog posts</a>";
 } else {
 #If no grade was assigned, display current grade (if any).
   echo "<form name='form' method='post' action=''>";
   echo "<input type='hidden' name='new' value='1' />";
   while ($row = mysqli_fetch_assoc($result)) {
     echo "<p><a href='" . $row['post_url'] . "' target='_blank'>" . $row['post_title'] . "</a></p>";
-    echo "<p>Grade <input name='grade' value='" . $row['grade'] . "' /> 3=Excellent, 2=OK, 1=Needs Work</p>";
-    echo "<p>Notes <input name='notes' value='" . $row['notes'] . "' /></p>";
+    echo "<p>Grade <input name='grade' size='5' value='" . $row['grade'] . "' /> 3=Excellent, 2=OK, 1=Needs Work</p>";
+    echo "<p>Notes <input name='notes' size='100' value='" . $row['notes'] . "' /></p>";
     echo "<input name='post_id' type='hidden' value='" . $row['post_id'] . "' />";
+    echo "<input name='blog_id' type='hidden' value='" . $row['blog_id'] . "' />";
   }
   echo "<p><input name='submit' type='submit' value='Update' /></p>";
   echo "</form>";
