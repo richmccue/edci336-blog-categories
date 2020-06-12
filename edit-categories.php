00f5c710e1350f3ed4a0f5c9cd9151af448dc643
Rich McCue <rich@msys.ca>
2020-06-12 16:06:37 -0700

    Edit categories working now

--- edit-categories.php
+++ edit-categories.php
@@ -16,8 +16,8 @@
  <head>
    <script type="text/javascript">
      window.onload = function() {
-       document.getElementById("grade").focus();
-       document.getElementById("grade").select();
+       document.getElementById("categories").focus();
+       document.getElementById("categories").select();
      };
    </script>
  </head>
@@ -33,22 +33,21 @@ echo "<form name='form' method='post' action=''>";
 
 # If the form was submitted update posts table with the grade assigned.
 if(isset($_POST['new']) && $_POST['new']==1) {
-  $update = "UPDATE posts SET grade=" . $_REQUEST['grade'] . ", notes='" . $_REQUEST['notes'] .
+  $update = "UPDATE posts SET categories='" . $_REQUEST['categories'] .
     "' WHERE post_id=" . $_REQUEST['post_id'];
-  mysqli_query($db, $update) or die(mysqli_error());
+  mysqli_query($db, $update) or die(mysqli_error($db));
   echo "<p style='color:#FF0000;''>Record Updated Successfully!</p>";
   echo "<a href='harvest-orig.php?id=" . $blog_id . "'>Return to student's blog posts</a>";
   ?>
     <script> window.location.href = "<? echo "harvest-orig.php?id=" . $blog_id  ?>" </script>;
   <?php
 } else {
-#If no grade was assigned, display current grade (if any).
+  #If no new category assigned, display current category.
   echo "<form name='form' method='post' action=''>";
   echo "<input type='hidden' name='new' value='1' />";
   while ($row = mysqli_fetch_assoc($result)) {
     echo "<p><a href='" . $row['post_url'] . "' target='_blank'>" . $row['post_title'] . "</a></p>";
-    echo "<p>Note <input name='grade' id='grade' size='5' value='" . $row['grade'] . "' /> 3=Excellent, 2=OK, 1=Needs Work</p>";
-    echo "<p>Additional Notes <input name='notes' size='100' value='" . $row['notes'] . "' /></p>";
+    echo "<p>Category <input name='categories' id='categories' size='80' value='" . $row['categories'] . "' /></p>";
     echo "<input name='post_id' type='hidden' value='" . $row['post_id'] . "' />";
     echo "<input name='blog_id' type='hidden' value='" . $row['blog_id'] . "' />";
   }
